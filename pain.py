de2f461263895c288298cc94f81f261c00379c9f
Homan Mirgolbabaee <homanmirgolbabaee@gmail.com>
2024-03-06 14:58:33 +0100

    pain.py

--- pain.py
+++ pain.py
@@ -1,16 +1,21 @@
 import streamlit as st
 import os
 from PIL import Image
-
+from PIL import Image, ImageDraw
 # Set the directory where your images are stored
 image_dir = 'images'
 
 
 
 import requests
+import json
+
+BRAIN_HEADER = st.secrets["brain_api"]
 
 API_URL = "https://api-inference.huggingface.co/models/DunnBC22/yolos-tiny-Brain_Tumor_Detection"
-headers = {"Authorization": "Bearer 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"}
+headers = {"Authorization": BRAIN_HEADER}
+
+
 
 def query(filename):
     with open(filename, "rb") as f:
@@ -22,6 +27,16 @@ def query(filename):
 
 
 
+def draw_boxes(image_path, boxes):
+    """
+    Draw rectangles on the image for each box in boxes.
+    """
+    with Image.open(image_path) as im:
+        draw = ImageDraw.Draw(im)
+        for box in boxes:
+            xmin, ymin, xmax, ymax = box
+            draw.rectangle(((xmin, ymin), (xmax, ymax)), outline="red", width=3)
+        return im
 
 
 
@@ -39,7 +54,7 @@ def dummy_operation():
     """
     A dummy operation function that returns a message.
     """
-    return "The button was clicked!"
+    return "Analysis Started ..."
 
 def main():
     st.sidebar.title('Radiology Image Viewer Controls')
@@ -62,6 +77,30 @@ def main():
                 output = query(selected_image)
                 st.write(message)
                 st.write(output)
+                # Extract bounding box coordinates from the output
+                boxes = []
+                if output:
+                    for result in output:
+                        box = result.get("box", {})
+                        xmin, ymin, xmax, ymax = box.get("xmin"), box.get("ymin"), box.get("xmax"), box.get("ymax")
+                        boxes.append((xmin, ymin, xmax, ymax))
+
+
+               # Draw boxes on the image
+                if boxes:
+                    image_with_boxes = draw_boxes(selected_image, boxes)
+                    st.image(image_with_boxes, caption=os.path.basename(selected_image), use_column_width=True)
+                else:
+                    # Display the original selected image in main area if there are no boxes
+                    image = Image.open(selected_image)
+                    st.image(image, caption=os.path.basename(selected_image), width=260)
+
+
+                # Write output to JSON file
+                with open('output.json', 'w') as f:
+                    json.dump(output, f)
+                
+                    
     else:
         st.sidebar.error('No images found in the specified directory. Please check the directory path.')
         
